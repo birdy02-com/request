ec56463bb6fa8bab4ee62ec9a4430665bef2ee13
林乐天 <81403750+birdy02-com@users.noreply.github.com>
2024-11-29 12:57:12 +0800

    Update request.go

--- request.go
+++ request.go
@@ -56,15 +56,15 @@ func GetRequestGetArg(baseurl string, args GetRequest) (*GetRequest, http.Client
 		reqArg.Stream = args.Stream
 	}
 
-	proxyURL, err := url.Parse("http://127.0.0.1:8080")
-	if err != nil {
-	}
+	//proxyURL, err := url.Parse("http://127.0.0.1:8080")
+	//if err != nil {
+	//}
 
 	// 请求参数设置
 	// 创建一个自定义的Transport，并禁用证书验证
 	transport := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		Proxy:           http.ProxyURL(proxyURL),
+		//Proxy:           http.ProxyURL(proxyURL),
 	}
 	// 设置Params
 	params := url.Values{}
